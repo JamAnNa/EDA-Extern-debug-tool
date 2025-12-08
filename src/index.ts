ac7d2bf3137ef37ae8e6668932ee6924a68a3486
IsAnAn <3599766025@qq.com>
2025-12-08 17:06:52 +0800

    初始修改，干翻AD的第一步

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import * as extensionConfig from '../extension.json';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function activate(status?: 'onStartupFinished', arg?: string): void {}
+
+export function about(): void {
+	eda.sys_Dialog.showInformationMessage(
+		eda.sys_I18n.text('EasyEDA extension SDK v', undefined, undefined, extensionConfig.version),
+		eda.sys_I18n.text('About'),
+	);
+}
+
+export function debug(): void{
+	eda.sys_IFrame.openIFrame("iframe/debug/index.html",800,500,"debug",{maximizeButton:true,minimizeButton:true});
+}
